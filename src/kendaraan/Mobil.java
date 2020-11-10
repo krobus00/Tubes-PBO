98d28414dbb512e45bba87e1b8c9d710e22760a1
yogifirgiawan <yogifirgiawan10@gmail.com>
2020-11-10 21:37:20 +0700

    Cuy Periksa punya gua dah

---
+++ src/kendaraan/Mobil.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kendaraan;
+
+/**
+ *
+ * @author REPUBLIC OF GAMERS
+ */
+public class Mobil extends kendaraan{
+    protected String merk;
+    protected date tanggal; 
+
+    public Mobil(String nomor_kendaraan) {
+    }
+    public void tambahKerusakan(String kerusakan, int biaya){
+    
+    }
+    public void updateKerusakan(String bagian, String status){
+    
+    }
+    public void daftarKerusakan(){
+    
+    }
+    public void detailKendaraan(){
+    }
+    
+}
